ec0935f60f7c6e933338e83b634e0801cd6bd72e
Borin Ouch <borin.ouch@gmail.com>
2024-12-14 19:50:39 -0800

    Implement day7

--- day7.py
+++ day7.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
+import ctypes
+import string
+
+
 s = """123 -> x
 456 -> y
 x AND y -> d
@@ -12,36 +16,110 @@ NOT x -> h
 NOT y -> i"""
 
 
-ids = {}
+
+def and_func(a, b):
+    return ctypes.c_uint16(a & b).value
 
 
-class BinaryOp(object):
-    def __init__(self, op, left, right):
-        self._op = op
-        self._left = left
-        self._right = right
-    def val(self):
-        return self._op(self._left(), self._right())
-
-class UnaryOp(object):
-    def __init__(self, op, val):
-        self._op = op
-        self._val = val
-    def val(self):
-        return self._op(self._val())
+def or_func(a, b):
+    return ctypes.c_uint16(a | b).value
 
 
-
-def tokenize(item):
-    try:
-        n = int(item, 10)
-        return Signal(n)
-    except:
-        if item == '->':
-            return 
+def lshift_func(a, b):
+    return ctypes.c_uint16(a << b).value
 
 
-for line in s.split("\n"):
-    tokens = map(tokenize, line.split(" "))
+def rshift_func(a, b):
+    return ctypes.c_uint16(a >> b).value
 
+
+def not_func(a):
+    return ctypes.c_uint16(~a).value
+
+
+def is_var(a):
+    return all(c in string.ascii_lowercase for c in a)
+
+
+def is_op(a):
+    return all(c in string.ascii_uppercase for c in a)
+
+
+ops = {
+    "AND": and_func,
+    "OR": or_func,
+    "LSHIFT": lshift_func,
+    "RSHIFT": rshift_func,
+    "NOT": not_func,
+}
+
+
+def parse_arg(arg):
+    if is_var(arg):
+        return ("var", arg)
+    elif is_op(arg):
+        return ("op", ops[arg])
+    else:
+        return ("int", int(arg, 10))
+
+
+def eval_int_or_var(arg, vars):
+    type_, val = arg
+
+    if type_ == "int":
+        return val
+
+    if val in vars["__cache__"]:
+        return vars["__cache__"][val]
+
+    res = vars[val]()
+    vars["__cache__"][val] = res
+
+    return res
+
+
+def make_func(ops, vars):
+    args = [parse_arg(arg) for arg in ops.strip().split(" ")]
+
+    def func():
+        if len(args) == 1:
+            arg, = args
+            return eval_int_or_var(arg, vars)
+
+        elif len(args) == 2:
+            (_, op_func), arg = args
+            return op_func(eval_int_or_var(arg, vars))
+
+        elif len(args) == 3:
+            a, (_, op_func), b = args
+            a_val = eval_int_or_var(a, vars)
+            b_val = eval_int_or_var(b, vars)
+            return op_func(a_val, b_val)
+
+        else:
+            raise NotImplementedError(f"Invalid args: {args}")
+    return func
+
+
+def parse(lines):
+    vars = { "__cache__": {} }
+
+    for line in lines:
+        ops, target = line.strip().split(" -> ")
+        vars[target] = make_func(ops, vars)
+
+    return vars
+
+
+print(parse(s.strip().split("\n"))["i"]())
+
+with open("day7.input") as f:
+    lines = f.read().strip().split("\n")
+    vars =  parse(lines)
+    part1 = vars["a"]()
+    print("part1: ", part1)
+
+    vars["__cache__"] = { "b": part1 }
+    part2 = vars["a"]()
+    print("part2: ", part2)
 
