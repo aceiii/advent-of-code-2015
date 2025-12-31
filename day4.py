90ab0e5e2a923a4e5ca0124cbade044226fbd3dd
Borin Ouch <borin.ouch@gmail.com>
2025-12-30 20:02:03 -0800

    Fix to day4 to run with python3, implement part 2

--- day4.py
+++ day4.py
@@ -1,23 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 
-import md5
+import hashlib
 
 
 s = "iwrupvqb"
 
 i = 1
 
-found = False
-while not found:
-    m = md5.new(s + str(i))
-    h = m.hexdigest()
-    if h[0:5] == "000000":
-        found = True
-        print("Found: " + str(i))
-    else:
-        i += 1
-        #print(h, i)
+part1 = None
+part2 = None
+prefix = s.encode('ascii')
+while part1 is None or part2 is None:
+    m = hashlib.md5(prefix + str(i).encode('ascii'))
+    h = m.digest()
+
+    a = int.from_bytes(h[:3])
+
+    if a <= 15 and part1 is None:
+        part1 = i
+        print("Part1: ", i)
+    if a == 0 and part2 is None:
+        part2 = i
+        print("Part2: ", i)
+
+    i += 1
+    #print(h, i)
 
 
