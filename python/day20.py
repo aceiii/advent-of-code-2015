84b64f5264f3f12f0550ad4672de43a1784e5c19
Borin Ouch <borin.ouch@gmail.com>
2026-01-24 10:27:07 -0800

    Implement day20 part 1

---
+++ python/day20.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def part1(lines):
+    target = int(lines[0],10)
+    arr = [0] * (target//10)
+    n = len(arr)
+    for i in range(1, n):
+        j = i
+        k = i * 10
+        while j < n:
+            arr[j] += k
+            j += i
+        if arr[i] >= target:
+            return i
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    lines = sys.stdin.read().strip().split("\n")
+    print("Part1: {}".format(part1(lines)))
+    print("Part2: {}".format(part2(lines)))
+
+
+if __name__ == "__main__":
+    main()
+
